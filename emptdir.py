e9f3b30f599dd3e891e4709ce5e126ece846ec46
Alexey N Surin <a@surin.ru>
2023-06-30 08:29:57 +0300

    Create emptdir.py

---
+++ emptdir.py
@@ -0,0 +1,14 @@
+# <!-- Alexey Surin | https://surin.ru | a@surin.ru | t.me/zelib0ba | +7(903) 153-51-56 -->
+# удаление пустых папок на диске
+import os
+def del_empty_dirs(path):
+    
+    for d in os.listdir(path):
+        a = os.path.join(path, d)
+        if os.path.isdir(a):
+            del_empty_dirs(a)
+            if not os.listdir(a):
+                os.rmdir(a)
+                print (f'папка {a} удалена')
+
+del_empty_dirs("p:\\")
